beb8ecb29bc742d84929416bee5215b02be78ae4
Ringin Restu Pati <ringinrp21@gmail.com>
2023-03-06 08:05:39 +0700

    update env

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL,
+  baseURL: "housy-deploy-production.up.railway.app/api/v1"
   // baseURL: "http://localhost:5000/api/v1",
 });
 
